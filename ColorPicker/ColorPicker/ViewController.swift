e3a261616e911f2b0258a06cb6d8ef43bc185486
Henry McMurray <hamz8f@mail.missouri.edu>
2019-10-25 23:23:05 -0500

    finished

--- ColorPicker/ColorPicker/ViewController.swift
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        cell.textLabel?.text = colors[indexPath.row]
+        
+        
+        return cell
     }
 
 
